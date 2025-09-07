18027cbcab4a739b377ef78a8c4dbe3f940e49fe
NOBITA <nobitaanetwork@gmail.com>
2025-09-07 21:58:26 +0530

    Add files via upload

---
+++ AnonXMusic/utils/inline/play.py
@@ -0,0 +1,154 @@
+import math
+
+from pyrogram.types import InlineKeyboardButton
+
+from AnonXMusic.utils.formatters import time_to_seconds
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+
+
+def track_markup(_, videoid, user_id, channel, fplay):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["P_B_1"],
+                callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",
+            ),
+            InlineKeyboardButton(
+                text=_["P_B_2"],
+                callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {videoid}|{user_id}",
+            )
+        ],
+    ]
+    return buttons
+
+
+def stream_markup_timer(_, chat_id, played, dur):
+    played_sec = time_to_seconds(played)
+    duration_sec = time_to_seconds(dur)
+    percentage = (played_sec / duration_sec) * 100
+    anon = math.floor(percentage)
+    
+    progress_bars = [
+        "⚪─────────", "━⚪────────", "━━⚪───────", "━━━⚪──────", "━━━━⚪─────",
+        "━━━━━⚪────", "━━━━━━⚪───", "━━━━━━━⚪──", "━━━━━━━━⚪─", "━━━━━━━━━⚪﻿"
+    ]
+    
+    ba = progress_bars[min(anon // 10, 9)]
+##bar of wynk---------------------------------------
+    
+    
+    
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=f"{played} {ba} {dur}",
+                callback_data="GetTimer",
+            )
+        ],
+        [
+            InlineKeyboardButton(text="◁ 20s", callback_data=f"seekback {chat_id} 20"),
+            InlineKeyboardButton(text="10s", callback_data=f"seekback {chat_id} 10"),
+            # InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="10s", callback_data=f"seek {chat_id} 10"),
+            InlineKeyboardButton(text="20s ▷", callback_data=f"seek {chat_id} 20"),
+        ],
+        [
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"), 
+            InlineKeyboardButton(
+                text="Skip", callback_data=f"ADMIN Skip|{chat_id}"
+            ),
+        ],
+        
+        # [
+        #     InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"), 
+        # ],
+    ]
+    return buttons
+
+
+
+
+def stream_markup(_, chat_id):
+    buttons = [
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+    ]
+    return buttons
+
+
+def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["P_B_1"],
+                callback_data=f"AviaxPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+            ),
+            InlineKeyboardButton(
+                text=_["P_B_2"],
+                callback_data=f"AviaxPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {videoid}|{user_id}",
+            ),
+        ],
+    ]
+    return buttons
+
+
+def livestream_markup(_, videoid, user_id, mode, channel, fplay):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["P_B_3"],
+                callback_data=f"LiveStream {videoid}|{user_id}|{mode}|{channel}|{fplay}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {videoid}|{user_id}",
+            ),
+        ],
+    ]
+    return buttons
+
+
+def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
+    query = f"{query[:20]}"
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["P_B_1"],
+                callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",
+            ),
+            InlineKeyboardButton(
+                text=_["P_B_2"],
+                callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="◁",
+                callback_data=f"slider B|{query_type}|{query}|{user_id}|{channel}|{fplay}",
+            ),
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {query}|{user_id}",
+            ),
+            InlineKeyboardButton(
+                text="▷",
+                callback_data=f"slider F|{query_type}|{query}|{user_id}|{channel}|{fplay}",
+            ),
+        ],
+    ]
+    return buttons
---
+++ AnonXMusic/utils/inline/start.py
@@ -0,0 +1,39 @@
+from pyrogram.types import InlineKeyboardButton
+
+import config
+from AnonXMusic import app
+
+
+def start_panel(_):
+    buttons = [
+        
+        [
+            InlineKeyboardButton(
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+            ),   
+        ],
+        [
+            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
+        ],
+    ]
+    return buttons
+
+
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(text="Help", callback_data="settings_back_helper"),
+            InlineKeyboardButton(text="New Feacture", url=f"https://telegra.ph/New-Features-08-31"),
+        ], 
+        [
+            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
+        ],
+        
+    ]
+    return buttons
