6887d079437216d1ceecf84afaa7f8c4e3ece853
NOBITA <nobitaanetwork@gmail.com>
2025-09-07 21:27:28 +0530

    Add files via upload

---
+++ docker.sh
@@ -0,0 +1,29 @@
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin git vim curl  -y
+sleep 2
+echo "Calling Docker Image"
+sleep 2
+
+docker build -t wyzz .
+docker container rm wyzz --force &
+sleep 3
+docker run -d --restart=unless-stopped --name wyzz wyzz
+
+sleep 2
+echo "Deployed Running....."
+
+sleep 5 
+echo "Bot is Started ✅"
